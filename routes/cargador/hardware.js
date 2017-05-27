359a55b950c842ecc3fafef36109f8560cd2a1c1
Willington Velasco <wito@tnmlimited.com>
2017-05-27 00:52:24 -0500

    implemetacion de put

--- routes/cargador/hardware.js
+++ routes/cargador/hardware.js
@@ -3,17 +3,60 @@ var router = express.Router();
 var Hardware = require('../../models/hardware')
 
 function findHardware(req, res, next){
-  //Hardware.findOne({codigo : req.params.device_address || req.body.device_address}, function(err, data){
-  Hardware.findOne({nombre : req.params.nombre}, function(err, data){
-console.log(req.params);
+  Hardware.find({codigo : req.params.device_address || req.body.device_address}, function(err, data){
+  //Hardware.findOne({nombre : req.params.nombre || req.body.nombre }, function(err, data){
+  //console.log('----buscando---');
+  //console.log(req.params);
     if(err) {
       res.status(500);
       res.json({error : 'Error looking for object'})
     }
     else {
       req.hardware = data
-      console.log(data);
+      //console.log('---- objeto encontrado ---');
+      //console.log(data);
       next();
+    }
+  })
+}
+function findAndSave(req, res, next){
+  Hardware.findOne({nombre : req.params.nombre || req.body.nombre }, function(err, data){
+    if(data ==null) {
+      res.status(500);
+      res.json({error : 'Error looking for object'})
+    }
+    else {
+      // se que hay una forma mas elegante de hacerlo pero no la he descubierto
+      //
+
+      var upHardware = new Hardware(data)
+      var newData
+      var updated
+      var i=0
+      for (var key in req.body ){
+          i++;
+      }
+      if (i<6){
+        updated = req.body
+        newData = data
+
+        if (typeof  updated.voltaje != 'undefined' ){
+          newData.voltaje[updated.voltaje.numero-1].numero=updated.voltaje.numero
+          newData.voltaje[updated.voltaje.numero-1].valor=updated.voltaje.valor
+        }
+        if (typeof  updated.corriente != 'undefined' ){
+          newData.corriente[updated.corriente.numero-1].numero=updated.corriente.numero
+          newData.corriente[updated.corriente.numero-1].valor=updated.corriente.valor
+        }
+
+      }else {  // llego completo
+        newData = req.body
+      }
+
+      upHardware.update(newData, function (err){
+        if (err) console.log(err);
+        next();
+      })
     }
   })
 }
@@ -35,6 +78,7 @@ router.get("/", function(req, res){
 })
 
 router.post("/", findHardware, function(req, res){
+
 
   if(req.hardware == null){
     nuevo_hardware = new Hardware()
@@ -65,6 +109,26 @@ router.post("/", findHardware, function(req, res){
     res.json({
       status : "error",
       message : "object with id " + req.body.nombre + " already exist"
+    })
+  }
+})
+
+router.put("/:name", findAndSave, function(req, res){
+  if(req.status == 500){
+    res.status(409)
+    res.json({
+      status : "error",
+      message : "object with Name " + req.body.nombre + " not exist"
+    })
+
+  }
+  else {
+
+    res.status(201)
+    res.json({
+      status : "success",
+      //payload : req.body,
+      message : "object with id " + (req.params.nombre || req.body.nombre) + " Updated"
     })
   }
 })
